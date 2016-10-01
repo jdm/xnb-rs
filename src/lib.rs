c4a8cee87b547d95ad231751ae2be5eb6b6a32c0
Josh Matthews <josh@joshmatthews.net>
2016-09-30 20:04:58 -0400

    Use byteorder instead of handwritten reader helpers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+extern crate byteorder;
+
+use byteorder::{ReadBytesExt, LittleEndian};
 use std::io::{Read, Result as IoResult, Error as IoError};
 
 pub struct XNB;
@@ -14,24 +17,12 @@ impl From<IoError> for Error {
     }
 }
 
-fn read_byte<R: Read>(rdr: &mut R) -> Result<u8, Error> {
-    let mut byte = vec![0];
-    try!(rdr.read_exact(&mut byte));
-    Ok(byte[0])
-}
-
-fn read_u32<R: Read>(rdr: &mut R) -> Result<u32, Error> {
-    let mut byte = vec![0, 0, 0, 0];
-    try!(rdr.read_exact(&mut byte));
-    Ok(byte[0] as u32 | (byte[1] as u32) << 8 | (byte[2] as u32) << 16 | (byte[3] as u32) << 24)
-}
-
 #[allow(dead_code)]
 fn read_7bit_encoded_int<R: Read>(rdr: &mut R) -> Result<u8, Error> {
     let mut result = 0;
     let mut bits_read = 0;
     loop {
-        let value = try!(read_byte(rdr));
+        let value = try!(rdr.read_u8());
         result |= (value & 0x7F) << bits_read;
         bits_read += 7;
         if value & 0x80 == 0 {
@@ -83,23 +74,23 @@ impl XNB {
         if header != b"XNB" {
             return Err(Error::Void);
         }
-        let target = try!(read_byte(&mut rdr));
+        let target = try!(rdr.read_u8());
         if ['w', 'm', 'x'].iter().find(|&b| *b == target as char).is_none() {
             return Err(Error::Void);
         }
 
-        let version = try!(read_byte(&mut rdr));
+        let version = try!(rdr.read_u8());
         if version != 5 {
             return Err(Error::Void);
         }
 
-        let flag = try!(read_byte(&mut rdr));
+        let flag = try!(rdr.read_u8());
         let is_compressed = flag & 0x80 != 0;
 
-        let _compressed_size = try!(read_u32(&mut rdr));
+        let _compressed_size = try!(rdr.read_u32::<LittleEndian>());
         
         if is_compressed {
-            let _decompressed_size = try!(read_u32(&mut rdr));
+            let _decompressed_size = try!(rdr.read_u32::<LittleEndian>());
             let buffer = try!(XNB::decompress(rdr));
             XNB::from_uncompressed_buffer(StringReader::new(buffer.as_bytes()))
         } else {
