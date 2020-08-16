8d61924be75a74993374e9b9440f19762932fc17
Josh Matthews <josh@joshmatthews.net>
2020-08-16 13:46:54 -0400

    Update xnbdump example.

--- examples/xnbdump.rs
+++ examples/xnbdump.rs
@@ -2,21 +2,50 @@ extern crate image;
 extern crate squish;
 extern crate xnb;
 
-use image::{DynamicImage, ImageBuffer, ImageFormat};
+use image::{DynamicImage, ImageBuffer};
 use squish::{decompress_image, CompressType};
 use std::env;
 use std::fs::File;
 use std::io::BufReader;
 use std::process;
-use xnb::{tide, Asset, SurfaceFormat, Texture2d, XNB};
+use xnb::{/*tide,*/ SurfaceFormat, Texture2d, XNB};
 
 fn usage() {
-    println!("xnbdump [file.xnb]");
+    println!("xnbdump [file.xnb] [type]");
     err()
 }
 
 fn err() {
     process::exit(1);
+}
+
+trait Dumpable {
+    fn dump(self);
+}
+
+impl Dumpable for xnb::Texture2d {
+    fn dump(self) {
+        dump_texture(self);
+    }
+}
+
+impl<T: std::fmt::Debug> Dumpable for Vec<T> {
+    fn dump(self) {
+        print!("[");
+        for val in self {
+            println!("{:?}, ", val);
+        }
+        print!("]");
+    }
+}
+
+fn dump_xnb<T: xnb::Parse + Dumpable>(xnb: xnb::MaybeCompressedXNB) -> Result<(), xnb::Error> {
+    let xnb: XNB<T> = match xnb {
+        xnb::MaybeCompressedXNB::Uncompressed(xnb) => xnb.xnb()?,
+        xnb::MaybeCompressedXNB::Compressed(xnb) => xnb.xnb(xnb::WindowSize::KB64)?,
+    };
+    xnb.primary.dump();
+    Ok(())
 }
 
 fn main() {
@@ -26,6 +55,10 @@ fn main() {
         Some(path) => path,
         None => return usage(),
     };
+    let typ = match args.next() {
+        Some(typ) => typ,
+        None => return usage(),
+    };
     let f = match File::open(&path) {
         Ok(f) => f,
         Err(e) => {
@@ -33,8 +66,8 @@ fn main() {
             return err();
         }
     };
-    let rdr = BufReader::new(f);
-    let xnb = match XNB::from_buffer(rdr) {
+    let mut rdr = BufReader::new(f);
+    let xnb = match xnb::MaybeCompressedXNB::from_buffer(&mut rdr) {
         Ok(xnb) => xnb,
         Err(e) => {
             println!("Error parsing file contents: {:?}", e);
@@ -42,7 +75,18 @@ fn main() {
         }
     };
 
-    match xnb.primary {
+    let result = match &*typ {
+        "texture2d" => dump_xnb::<xnb::Texture2d>(xnb),
+        "stringarray" => dump_xnb::<Vec<String>>(xnb),
+        typ => unimplemented!("No support for \"{}\" XNBs", typ),
+    };
+
+    if let Err(e) = result {
+        println!("Error dumping {}: {:?}", typ, e);
+        return err();
+    }
+
+    /*match xnb.primary {
         Asset::Null => (),
 
         Asset::Texture2d(texture) => {
@@ -136,41 +180,31 @@ fn main() {
                 }
             }
         }
-    }
+    }*/
 }
 
 fn dump_texture(texture: Texture2d) {
     for (i, data) in texture.mip_data.into_iter().enumerate() {
         let path = format!("data_{}.png", i);
-        match File::create(&path) {
-            Ok(mut f) => {
-                let dynamic_image = {
-                    let data = match texture.format {
-                        SurfaceFormat::Color => data,
-                        SurfaceFormat::Dxt3 => decompress_image(
-                            texture.width as i32,
-                            texture.height as i32,
-                            data.as_ptr() as *const _,
-                            CompressType::Dxt3,
-                        ),
-                        f => panic!("can't handle surface format {:?}", f),
-                    };
+        let dynamic_image = {
+            let data = match texture.format {
+                SurfaceFormat::Color => data,
+                SurfaceFormat::Dxt3 => decompress_image(
+                    texture.width as i32,
+                    texture.height as i32,
+                    data.as_ptr() as *const _,
+                    CompressType::Dxt3,
+                ),
+                f => panic!("can't handle surface format {:?}", f),
+            };
 
-                    let img =
-                        ImageBuffer::from_raw(texture.width as u32, texture.height as u32, data)
-                            .unwrap();
-                    DynamicImage::ImageRgba8(img)
-                };
-                if let Err(e) = dynamic_image.save(&mut f, ImageFormat::PNG) {
-                    println!("Error saving PNG: {}", e);
-                    return err();
-                }
-            }
-
-            Err(e) => {
-                println!("Error creating file {}: {}", path, e);
-                return err();
-            }
+            let img =
+                ImageBuffer::from_raw(texture.width as u32, texture.height as u32, data).unwrap();
+            DynamicImage::ImageRgba8(img)
+        };
+        if let Err(e) = dynamic_image.save(path) {
+            println!("Error saving PNG: {}", e);
+            return err();
         }
     }
 }
